be312dfd7d5db63998da7ed793175dce71969d16
Katrina Behnken <katrina.behnken@gmail.com>
2020-06-04 09:00:46 -0700

    Added mysql database connection

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+'use strict';
+
+require('dotenv').config();
 const express = require('express');
+const mysql = require('mysql')
+
 const app = express();
 const PORT = 4042;
-
+const { HOST, DB, DB_USERNAME, DB_PASSWORD } = process.env;
+const connection = mysql.createConnection({
+    host: HOST,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB
+})
+connection.connect(function(err) {
+    if(err) {
+        throw err;
+    } else {
+        console.log('DB connected');
+    }
+});
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
