e67a130ea327bb5656608fa142fae7493279a8b9
Katrina Behnken <katrina.behnken@gmail.com>
2020-08-31 10:26:53 -0700

    Added auth middleware code. Uncommented auth and user routes.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
-// require('dotenv').config();
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+const expressJwt = require('express-jwt');
+
 const PORT = 4042;
 
 // parse requests of content-type: application/json
@@ -12,28 +15,23 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// auth middleware
+const jwtOptions = {
+    secret: accessTokenSecret,
+    algorithms: ['HS256']
+}
+const unauthenticatedRoutes = {
+    path: ['/api/v1/login']
+}
+app.use(expressJwt(jwtOptions)
+.unless(unauthenticatedRoutes));
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-// const mysql = require('mysql')
-// const { HOST, DB, DB_USERNAME, DB_PASSWORD } = process.env;
-// const connection = mysql.createConnection({
-//     host: HOST,
-//     user: DB_USERNAME,
-//     password: DB_PASSWORD,
-//     database: DB
-// })
-// connection.connect(function(err) {
-//     if(err) {
-//         throw err;
-//     } else {
-//         console.log('DB connected');
-//     }
-// });
-
-// const authRte = require('./routes/authRoutes');
-// authRte(app);
-// const userRte = require('./routes/userRoutes.js');
-// userRte(app);
+const authRte = require('./routes/authRoutes');
+authRte(app);
+const userRte = require('./routes/userRoutes.js');
+userRte(app);
 const recipeRte = require('./routes/recipeRoutes.js');
 recipeRte(app);
 const ingredientRte = require('./routes/ingredientRoutes.js');
