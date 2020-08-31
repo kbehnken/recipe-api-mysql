41a77531678d154739a70cb16d5a88ada430fe1f
Katrina Behnken <katrina.behnken@gmail.com>
2020-08-31 10:31:29 -0700

    Initial commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+// const auth = require('../Middleware/authMiddleware');
+
+module.exports = app => {
+    const users = require('../controllers/userController.js');
+
+    // Create a new user profile
+    app.post('/api/v1/users', users.create);
+
+    // Return all user profiles
+    app.get('/api/v1/users', users.findAll);
+
+    // Return a single user profile with userId
+    app.get('/api/v1/users/:userId', users.findOne);
+
+    // Update a user profile with userId
+    app.put('/api/v1/users/:userId', users.update);
+
+    // Delete a user profile with userId
+    app.delete('/api/v1/users/:userId', users.delete);
+
+};
