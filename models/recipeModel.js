f324dad6bdc84876be73c64fb483398518e6d848
Katrina Behnken <katrina.behnken@gmail.com>
2020-08-31 10:36:35 -0700

    Updated sql query in findById function.

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -3,10 +3,11 @@ const sql = require('./db.js');
 // constructor
 const Recipe = function(recipe) {
   this.recipe_name = recipe.recipe_name;
-  this.contributor = recipe.contributor;
+  this.author = recipe.author;
   this.photo_url = recipe.photo_url;
   this.prep_time = recipe.prep_time;
   this.cook_time = recipe.cook_time;
+  this.directions = recipe.directions;
   this.created_at = new Date();
 };
 
@@ -24,7 +25,21 @@ Recipe.create = (newRecipe, result) => {
 };
 
 Recipe.findById = (recipe_id, result) => {
-  sql.query(`SELECT * FROM recipes WHERE recipe_id = ${recipe_id}`, (err, res) => {
+  sql.query(`SELECT r.recipe_name, r.author, r.photo_url, r.prep_time, r. cook_time, r. directions, i.ingredient_name, t.tag, i_to_r.quantity 
+                FROM recipes AS r           
+                LEFT JOIN 
+                  ingredients_to_recipes AS i_to_r 
+                  ON r.recipe_id = i_to_r.recipe_id
+                LEFT JOIN
+                  ingredients AS i
+                  ON i.ingredient_id = i_to_r.ingredient_id
+                LEFT JOIN
+                  tags_to_recipes AS t_to_r
+                  ON r.recipe_id = t_to_r.recipe_id
+                LEFT JOIN
+                  tags AS t
+                  ON t.tag_id = t_to_r.tag_id 
+                WHERE r.recipe_id = ${recipe_id}`, (err, res) => {
     if (err) {
       console.log('error: ', err);
       result(err, null);
@@ -32,8 +47,8 @@ Recipe.findById = (recipe_id, result) => {
     }
 
     if (res.length) {
-      console.log('found recipe: ', res[0]);
-      result(null, res[0]);
+      // console.log('found recipe: ', res);
+      result(null, res);
       return;
     }
     // Recipe ID not found
