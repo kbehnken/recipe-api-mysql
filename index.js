0ee3fc9cb46972029c006e4399dc5ad7fa505ca1
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-21 10:31:12 -0700

    Uncomment userRte and delete ingredientRte

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 const expressJwt = require('express-jwt');
-
 const PORT = 4042;
 
 // Parse requests of content-type: application/json
@@ -34,11 +33,9 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 // Routes
 const authRte = require('./routes/authRoutes');
 authRte(app);
-// const userRte = require('./routes/userRoutes.js');
-// userRte(app);
+const userRte = require('./routes/userRoutes.js');
+userRte(app);
 const recipeRte = require('./routes/recipeRoutes.js');
 recipeRte(app);
 const favRecipeRte = require('./routes/favoriteRecipeRoutes.js');
 favRecipeRte(app);
-// const ingredientRte = require('./routes/ingredientRoutes.js');
-// ingredientRte(app);
